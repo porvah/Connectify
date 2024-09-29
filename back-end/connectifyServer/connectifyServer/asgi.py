df6a406370b570e4b978a1a4ed77158ccbe6d6f3
Rowan mohammad <150041546+RowanYoussef@users.noreply.github.com>
2024-09-29 21:57:57 +0300

    consumer.py

--- back-end/connectifyServer/connectifyServer/asgi.py
+++ back-end/connectifyServer/connectifyServer/asgi.py
@@ -7,13 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+# your_project_name/asgi.py
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat_handler.consumers import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connectifyServer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application()
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
+
--- back-end/connectifyServer/connectifyServer/settings.py
+++ back-end/connectifyServer/connectifyServer/settings.py
@@ -56,6 +56,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'connectifyServer.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer', 
+    },
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
