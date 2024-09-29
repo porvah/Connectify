b6000eb8a54c71b9b6cd0dd7a30e8e512a067758
Rowan mohammad <150041546+RowanYoussef@users.noreply.github.com>
2024-09-29 22:23:41 +0300

    edit

--- back-end/connectifyServer/connectifyServer/asgi.py
+++ back-end/connectifyServer/connectifyServer/asgi.py
@@ -13,7 +13,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat_handler.consumers import websocket_urlpatterns
+from chat_handler.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
