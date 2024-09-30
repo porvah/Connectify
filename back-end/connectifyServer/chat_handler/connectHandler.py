5b58c14e307d1f1c7ca87a375d12d11945e73ee1
Rowan mohammad <150041546+RowanYoussef@users.noreply.github.com>
2024-09-30 18:44:32 +0300

    message - handler

--- back-end/connectifyServer/chat_handler/connectHandler.py
+++ back-end/connectifyServer/chat_handler/connectHandler.py
@@ -1,7 +1,12 @@
 from asgiref.sync import sync_to_async
+import uuid
 
 @sync_to_async
 def get_user_phone(token):
     from users.loggedusersmodel import LoggedUsersModel
     user = LoggedUsersModel.objects.filter(token=token).first()
     return user.phone
+
+def generateID():
+    message_id = str(uuid.uuid4())  
+    return message_id
--- back-end/connectifyServer/chat_handler/consumers.py
+++ back-end/connectifyServer/chat_handler/consumers.py
@@ -1,49 +1,39 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-from .connectHandler import get_user_phone
+from .connectHandler import get_user_phone,generateID
 
 # A global dictionary to store connected users by phone number
 connected_users = {}
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # The phone number will be passed through the URL when the user connects
         self.token = self.scope['url_route']['kwargs']['token']
         self.phone_number = await get_user_phone(self.token)
-        print(f"User with phone number {self.phone_number} is trying to connect.")
-
-        # Add the user's WebSocket channel to the connected_users dictionary
         connected_users[self.phone_number] = self.channel_name
-
-        # Accept the WebSocket connection
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Remove the user's connection from the connected_users dictionary on disconnect
         if self.phone_number in connected_users:
             del connected_users[self.phone_number]
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message = data['message']
-        sender = data['sender']
+        data['type'] = 'chat_message'
+        data['message_id'] = generateID()
         receiver = data['receiver']
+
+        await self.send(text_data=json.dumps({
+            'message_id' : data.get('message_id'),
+        }))
 
         # Check if the receiver is connected
         if receiver in connected_users:
-            # Send message directly to the receiver's WebSocket connection
             await self.channel_layer.send(
-                connected_users[receiver],  # The channel name of the receiver
-                {
-                    'type': 'chat_message',
-                    'message': message,
-                    'sender': sender,
-                }
+                connected_users[receiver],
+                data  
             )
         else:
-            # Handle case where the receiver is not connected (optional)
             await self.send(text_data=json.dumps({
                 'error': 'Receiver is not connected.',
             }))
@@ -51,12 +41,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Handle the incoming message and send it to the WebSocket client (receiver)
     async def chat_message(self, event):
-        message = event['message']
-        sender = event['sender']
-         
-        # Send the message to the WebSocket client (receiver)
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'sender': sender,
-        }))
+        del event['type']
+        await self.send(text_data=json.dumps(event))
         print("success")
