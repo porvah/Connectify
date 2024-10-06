dc937ebc38d2f4a7b6805e4e02397036ae43cb8c
Ayman Algamal <ayman01287985950@gmail.com>
2024-10-06 21:56:32 +0300

    Handling the replyingTo state in the chat screen.

--- client/connectify/lib/screens/chat_page.dart
+++ client/connectify/lib/screens/chat_page.dart
@@ -4,8 +4,10 @@ import 'package:Connectify/core/user.dart';
 import 'package:Connectify/utils/chatManagement.dart';
 import 'package:Connectify/widgets/MessageInput.dart';
 import 'package:Connectify/widgets/ReceivedMessage.dart';
+import 'package:Connectify/widgets/ReplyLabel.dart';
 import 'package:Connectify/widgets/SentMessage.dart';
 import 'package:Connectify/widgets/costumAppBar.dart';
+import 'package:Connectify/widgets/ReplyPreview.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -26,8 +28,7 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
   bool _isKeyboardVisible = false;
   bool _isLoading = false;
   bool _hasMore = true;
-  
-
+  Message? _replyingTo;
 
   @override
   void initState() {
@@ -42,25 +43,28 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
     // });
   }
 
-  void _scrollListener(){
-    if(_scrollController.hasClients){
-      if(_scrollController.position.pixels <= _scrollController.position.minScrollExtent+200 &&
-        !_isLoading && _hasMore){
-          print("extending triggered");
-          _extendMessages();
-        }
+  void _scrollListener() {
+    if (_scrollController.hasClients) {
+      if (_scrollController.position.pixels <=
+              _scrollController.position.minScrollExtent + 200 &&
+          !_isLoading &&
+          _hasMore) {
+        print("extending triggered");
+        _extendMessages();
+      }
     }
   }
 
-  Future<void> _extendMessages()async{
-    if(_isLoading || !_hasMore) return;
-    setState((){
+  Future<void> _extendMessages() async {
+    if (_isLoading || !_hasMore) return;
+    setState(() {
       _isLoading = true;
     });
     int offset = _messages.value.length;
-    List<Message> addedMessages = await ChatManagement.queryMessages(sender!.phone!, chat.phone!, offset);
-    if(addedMessages.isEmpty){
-      setState((){
+    List<Message> addedMessages =
+        await ChatManagement.queryMessages(sender!.phone!, chat.phone!, offset);
+    if (addedMessages.isEmpty) {
+      setState(() {
         _isLoading = false;
         _hasMore = false;
       });
@@ -68,26 +72,23 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
     }
     double currPos = _scrollController.position.pixels;
     double max = _scrollController.position.maxScrollExtent;
-    
+
     setState(() {
       _messages.value = List.from(_messages.value)..insertAll(0, addedMessages);
       _isLoading = false;
     });
-      
-    WidgetsBinding.instance.addPersistentFrameCallback((_){
+
+    WidgetsBinding.instance.addPersistentFrameCallback((_) {
       double max2 = _scrollController.position.maxScrollExtent;
-      _scrollController.jumpTo(currPos+(max2-max)); 
-
+      _scrollController.jumpTo(currPos + (max2 - max));
     });
-    
-
   }
 
   @override
   void didChangeMetrics() {
     super.didChangeMetrics();
     if (!mounted) return;
-    
+
     final bottomInset = View.of(context).viewInsets.bottom;
     final newValue = bottomInset > 0.0;
     if (newValue != _isKeyboardVisible) {
@@ -107,15 +108,16 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
     setState(() {
       _isLoading = true;
     });
-    List<Message> queried_m = await ChatManagement.queryMessages(sender!.phone!, chat.phone!, 0);
+    List<Message> queried_m =
+        await ChatManagement.queryMessages(sender!.phone!, chat.phone!, 0);
     setState(() {
       _isLoading = false;
     });
-      
+
     if (queried_m.length < 20) _hasMore = false;
     _messages.value = queried_m;
     ChatManagement.messages = _messages;
-    
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _scrollToBottom(animate: false);
     });
@@ -163,21 +165,43 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
                         final message = messages[index];
                         final timeFormatted = DateFormat('HH:mm a')
                             .format(DateTime.parse(message.time!));
-                            
-                        return message.receiver == chat.phone
-                            ? Sentmessage(
-                                message,
-                                timeFormatted,
-                              )
-                            : Receivedmessage(
-                                message,
-                                timeFormatted,
-                              );
+
+                        final repliedMessage = message.replied != null
+                            ? messages.firstWhere(
+                                (m) => m.id == message.replied,
+                                orElse: () => message)
+                            : null;
+
+                        return Column(
+                          crossAxisAlignment: message.receiver == chat.phone
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          children: [
+                            if (repliedMessage != null)
+                              ReplyLabel(repliedMessage: repliedMessage),
+                            message.receiver == chat.phone
+                                ? Sentmessage(
+                                    message,
+                                    timeFormatted,
+                                    onReply: _setReplyingTo,
+                                  )
+                                : Receivedmessage(
+                                    message,
+                                    timeFormatted,
+                                    onReply: _setReplyingTo,
+                                  ),
+                          ],
+                        );
                       },
                     );
                   },
                 ),
               ),
+              if (_replyingTo != null)
+                ReplyPreview(
+                  message: _replyingTo!,
+                  onCancel: () => _setReplyingTo(null),
+                ),
               Messageinput(_sendPhoto, _sendMessage, _controller),
             ],
           ),
@@ -197,6 +221,9 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
         _controller.text,
       );
 
+      if (_replyingTo != null) {
+        m.replied = _replyingTo!.id;
+      }
       ChatManagement.sendMessage(m);
 
       if (m.sender != m.receiver) {
@@ -207,10 +234,31 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
         _scrollToBottom(animate: true);
       });
     }
+    // DEBUGGING: Print all messages in the list
+    // for (Message m in _messages.value) {
+    //   print('Message ID: ${m.id}');
+    //   print('Sender: ${m.sender}');
+    //   print('Receiver: ${m.receiver}');
+    //   print('Replied To: ${m.replied}');
+    //   print('Time: ${m.time}');
+    //   print('Content: ${m.stringContent}');
+    //   print('Starred: ${m.starred}');
+    //   print('Attachment ID: ${m.attachment_id}');
+    //   print('-------------------'); // Separator for better readability
+    // }
+
+    // remove preview
+    _setReplyingTo(null);
   }
 
   void _sendPhoto() {
     // Implementation for sending photos
+  }
+
+  void _setReplyingTo(Message? message) {
+    setState(() {
+      _replyingTo = message;
+    });
   }
 
   @override
