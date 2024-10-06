f7a11daeccedf92edba2fafc162b4d59fa267ed3
RowanYoussef <150041546+RowanYoussef@users.noreply.github.com>
2024-10-07 00:15:58 +0300

    edit attachment field in backend

--- back-end/connectifyServer/chat_handler/migrations/0001_initial.py
+++ back-end/connectifyServer/chat_handler/migrations/0001_initial.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                 ('hasattachment', models.IntegerField(default=0, null=False)),
                 ('text', models.CharField(max_length=1000, null=True)),
                 ('attachment_name', models.CharField(max_length=255, null=True)),
-                ('attachment', models.CharField(max_length=255, null=True)),
+                ('attachment', models.TextField(null=True)),
                 ('replied', models.CharField(max_length=255,default="none",null = True)),
                 ('message_id', models.CharField(null = False)),
                 ('time', models.CharField(max_length=255, null=True)),
